8b9cf86eb2ef3853debaefb043c56520a99c3915
Dungph-DA/Dev <phanhuydungnq@gmail.com>
2026-03-14 23:01:04 +0700

    Add initial data inserts for Student, Professor, and Course

---
+++ B2_MD01_BKDA/Exercise2_B2_MD01.sql
@@ -0,0 +1,17 @@
+INSERT INTO dbo.Student(Student_ID, StudentName, StudentBirth, IsMale) VALUES (1, 'A', 2000-10-01, null),
+                                                                              (2, 'B', '2000-12-01', NULL),
+                                                                              (3, 'C', '2000-10-10', 0),
+                                                                              (4, 'D', '2000-08-20', 1),
+                                                                              (5, 'E', '2000-05-15', NULL),
+                                                                              (6, 'F', '2000-05-12', NULL);
+INSERT INTO dbo.Professor(Employee_ID, ProfessorName) VALUES (1, 'A'),
+                                                             (2, 'B'),
+                                                             (3, 'C'),
+                                                             (4, 'D');
+INSERT INTO dbo.Course(Course_ID, CourseName, Professor_ID) VALUES (1, 'thuật toán', 1),
+                                                                   (2, 'toán', 2),
+                                                                   (3, 'vật lý', 4);
+INSERT Into dbo.Assigned(Student_ID, Course_ID, Note) VALUES (1, 1, null),
+                                                             (1,2, 'Test'),
+                                                             (2,1, null),
+                                                             (2,3, null);
