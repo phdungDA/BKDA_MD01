d7e5a49d456d92b796143643eec4fb9229b7a37f
Dungph-DA/Dev <phanhuydungnq@gmail.com>
2026-03-20 19:10:04 +0700

    Modify Query3.sql to include Spanish and French names
    Updated query to include products with names starting with 'HL' in Spanish and French.

--- B3_MD01_BKDA/Query3.sql
+++ B3_MD01_BKDA/Query3.sql
@@ -1,3 +1,5 @@
 --Danh sách tất cả các sản phẩm có tên bắt đầu bằng chữ HL
 SELECT * FROM DimProduct
 WHERE EnglishProductName LIKE 'HL%'
+OR SpanishProductName LIKE 'HL%'
+OR FrenchProductName LIKE 'HL%'
