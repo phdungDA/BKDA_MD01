ede2f7ec1f1cbbb41c6834e702d21d615960bba3
Dungph-DA/Dev <phanhuydungnq@gmail.com>
2026-03-30 10:32:00 +0700

    Update Query_Character.sql

--- B5_MD01_BKDA/Query_Character.sql
+++ B5_MD01_BKDA/Query_Character.sql
@@ -8,13 +8,21 @@ WHERE DepartmentName = 'Marketing'
 
 --Exercise 2: Thay thế tên miền email của từng nhân viên phòng production thành production.com trong bảng DimEmployee 
 
-UPDATE DimEmployee
-SET EmailAddress = LEFT(EmailAddress, CHARINDEX('@', EmailAddress)) + 'production.com'
-WHERE DepartmentName = 'Production'
-
-select EmailAddress FROM DimEmployee
+select EmailAddress,
+REPLACE(EmailAddress, 'adventure-work.com', 'production.com')
+FROM DimEmployee
 WHERE DepartmentName = 'Production'
 
 /*Exercise 3: Thêm một cột trong bảng Product theo logic: 
   Nếu có Color thì lấy phần trước dấu – của ProductAlternateKey 
   Nếu không có Color thì lấy phần sau dấu – của ProductAlternateKey*/
+
+SELECT ProductKey, ProductAlternateKey, color,
+case
+    when Color is null or Color != 'NA'
+    then LEFT(ProductAlternateKey, CHARINDEX('-', ProductAlternateKey) - 1)
+else SUBSTRING(ProductAlternateKey, CHARINDEX('-', ProductAlternateKey) + 1, LEN(ProductAlternateKey))
+END AS DerivedColumn
+from DimProduct
+
+--
